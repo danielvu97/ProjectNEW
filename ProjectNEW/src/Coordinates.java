d201387bf0b3efeaef806e5b07459a2fc2a4fa85
Daniel <daniel.vu97@hotmail.com>
2020-05-27 15:19:44 +0200

    more walls

--- ProjectNEW/src/Coordinates.java
+++ ProjectNEW/src/Coordinates.java
@@ -4,12 +4,18 @@ import java.awt.geom.Area;
 public class Coordinates extends furpic {
 	
 	Rectangle [] save = new Rectangle[10];
-	//Rectangle [] walls = new Rectangle[10];
-	Rectangle wall = new Rectangle(); 
+	Rectangle [] obstacle = new Rectangle[12];
 	
 	public Coordinates() {
 		i = 0;
-		wall.setBounds(404, 76, 15, 100);
+		for(int l = 0; l<12; l++) {
+			obstacle[l] = new Rectangle();
+		}
+		obstacle[0].setBounds(293, 76, 18, 104); //window
+		obstacle[1].setBounds(293, 0, 5, 495); //left House wall
+		obstacle[2].setBounds(293, 0, 500, 5); //Upper House wall
+		obstacle[3].setBounds(785, 0, 5, 495); //Right House wall
+		obstacle[4].setBounds(293, 494, 500, 5); //Bottom House wall
 	}
 	
 	public void Coordinating(Rectangle A, String Name) {
@@ -22,8 +28,10 @@ public class Coordinates extends furpic {
 	public boolean compareCoordinates(Rectangle B, String Namn) {
 		i = Integer.parseInt(Namn);
 		
-		if(wall.intersects(B)) {
+		for(int k=0; k<obstacle.length; k++) {
+		if(obstacle[k].intersects(B) && obstacle[k] != null) {
 			return true;
+		}
 		}
 		
 		for(int j=0; j<10; j++) {
